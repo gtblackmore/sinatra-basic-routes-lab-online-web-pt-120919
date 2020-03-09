54d5cd127e399e96bf6835eab8fe4525d1a1d3c0
George Blackmore <gtblackmore@gmail.com>
2020-03-09 19:33:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,14 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get "/name" do
-    "My name is George"
+    "My name is __"
   end
   
   get "/hometown" do
-    "My hometown is Detroit"
+    "My hometown is __"
   end
   
   get "/favorite-song" do
-    "My favorite song is ____"
+    "My favorite song is __"
   end
 end
